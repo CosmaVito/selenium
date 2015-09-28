f07f91cf86616815bb22f1228801abcd10c5bbcd
piercolella <piercolella@gmail.com>
2015-09-28 16:17:28 +0200

    start

---
+++ GetPlaylist.py
@@ -0,0 +1,21 @@
+import re
+import mechanize
+from selenium  import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+class GetPlaylist(object):
+
+    def getTopSongs(self):
+        print "working billboard"
+        browser = webdriver.Firefox()
+        browser.get('http://www.billboard.com/charts/rock-songs')
+        time.sleep(5)
+        title = browser.find_element_by_xpath('.//div[1]/div[4]/h2').text
+        print title
+
+myClassObject = GetPlaylist()
+myClassObject.getTopSongs()
+
+
